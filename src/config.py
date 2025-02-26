59d8c3aa74995a4bcc031ad0d84995e6b030136d
JaniShreyas <janishreyas5@gmail.com>
2025-02-26 10:13:25 +0530

    Add RANDOM_STATE to config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 class DevConfig:
     ROOT_DIR = r"C:\Users\Jani\.cache\kagglehub\datasets\vineethakkinapalli\ucf50-action-recognition-dataset\versions\1\UCF50"
     MODELS_DIR_LOCAL = "outputs/models"
+    RANDOM_STATE = 42
