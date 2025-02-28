e1b7c614176243bc1294e6c64aa900d8df2d9acd
JaniShreyas <janishreyas5@gmail.com>
2025-02-28 17:33:39 +0530

    Update config file comment

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 class DevConfig:
     ROOT_DIR = r"C:\Users\Jani\.cache\kagglehub\datasets\vineethakkinapalli\ucf50-action-recognition-dataset\versions\1\UCF50"
     ANNOTATIONS_DIR_LOCAL = "annotations"
-    FULL_ANNOTATIONS_FILE_LOCAL = "annotations/annotations.csv"  # Locations of annotations directory in root directory
+    FULL_ANNOTATIONS_FILE_LOCAL = "annotations/annotations.csv" 
     TRAIN_ANNOTATIONS_FILE_LOCAL = "annotations/train_annotations.csv"
     VAL_ANNOTATIONS_FILE_LOCAL = "annotations/val_annotations.csv"
     TEST_ANNOTATIONS_FILE_LOCAL = "annotations/test_annotations.csv"
